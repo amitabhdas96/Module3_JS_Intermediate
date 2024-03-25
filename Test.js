f9396bffd029e4faa8323980e629c2fae0d60ca5
amitabhdas96 <amitabh.sweden@gmail.com>
2024-03-25 20:29:17 +1300

    okok

--- Test.js
+++ Test.js
@@ -1,28 +1,14 @@
-const today = new Date();
-console.log('Current time is ' + today.toLocaleTimeString())
-console.log(today.getHours() + ' hours have passed so far today')
-console.log((60 * today.getHours()) + ' minutes have passed so far today')
-console.log((60 * 60 * today.getHours()) + ' seconds have passed so far today')
+const user = {
+  name: "John",
+  sayHi() {
+    console.log(`Hi, ${this.name}`);
+  },
+};
+user.sayHi(); // called directly, works! Hi, John
+// setTimeout(user, 1000); // passed as reference, fails! Hi, undefined
 
-let years = 1984 - 2024;
-let months = 2 - 3;
-let days = 2 - 23;
-
-console.log('I am ' + -years + ' years, ' + -months + ' months and ' + -days + 'days old')
-
-
-let date1 = new Date("01/16/2024");
-let date2 = new Date("01/26/2024");
-
-let Difference_In_Time = date2.getTime() - date1.getTime();
-let Difference_In_Days = Math.round(Difference_In_Time / (1000 * 3600 * 24));
-
-console.log
-    ("Total number of days between dates:\n" +
-        date1.toDateString() + " and " +
-        date2.toDateString() +
-        " is: " + Difference_In_Days + " days");
-
-
-
-
+function isOdd(number) {
+  return number % 2;
+}
+a = [2,3, 4, 6, 8]
+console.log([10, 1, 3, 4, 8, 9].filter.call(a, isOdd));
